7d0ccb2958e285d59224240fab0883f5824f08e0
MossTeMuerA <emresevuk@gmail.com>
2023-01-05 18:08:03 +0300

    FirstCommit

---
+++ Assets/A1_Project/Scripts/A3_Level/LevelActor.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelActor : MonoBehaviour
+{
+    public LevelPreparationOfficer levelPreparationOfficer;
+    public LevelCollectedPointsOfficer levelCollectedPointsOfficer;
+}
