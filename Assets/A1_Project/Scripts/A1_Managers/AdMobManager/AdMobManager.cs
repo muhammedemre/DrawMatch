aa38fbb407e4881ab00a44052e317b5661b0fc42
Taylan Bartu Yoran <tbyoran@gmail.com>
2023-01-16 19:17:57 +0300

    updated AdMobManager

--- Assets/A1_Project/Scripts/A1_Managers/AdMobManager/AdMobManager.cs
+++ Assets/A1_Project/Scripts/A1_Managers/AdMobManager/AdMobManager.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 using GoogleMobileAds.Api;
+using GameAnalyticsSDK;
+using TMPro;
 
 public class AdMobManager : MonoBehaviour
 {
@@ -28,6 +30,9 @@ public class AdMobManager : MonoBehaviour
     private const string Rewarded_Hint_IOS = "ca-app-pub-4426570100779360/3798508154";
     private const string Interstitial_Level_IOS = "ca-app-pub-4426570100779360/7546181473";
     #endregion
+
+    private InterstitialAd interstitialAd;
+    private RewardedAd rewardedAd;
 
 
     private void Awake()
@@ -56,21 +61,38 @@ public class AdMobManager : MonoBehaviour
         MobileAds.SetRequestConfiguration(requestConfiguration);
 
         MobileAds.Initialize(initStatus => {
+            LoadInterstitialAd();
+            LoadRewardedAd();
         });
     }
 
-    public void ShowAd(InterstitialAdType adType)
+    private void ClearInterstitialAd()
     {
+        if (interstitialAd == null) return;
+
+        interstitialAd.Destroy();
+        interstitialAd = null;
+    }
+
+    private void ClearRewardedAd()
+    {
+        if (rewardedAd == null) return;
+
+        rewardedAd.Destroy();
+        rewardedAd = null;
+    }
+
+    private void LoadInterstitialAd()
+    {
+        ClearInterstitialAd();
+
         string adUnitId = "";
 
-        if (adType == InterstitialAdType.Level)
-        {
 #if UNITY_ANDROID
-            adUnitId = Interstitial_Level_Android;
+        adUnitId = Interstitial_Level_Android;
 #elif UNITY_IOS
-            adUnitId = Interstitial_Level_IOS;
+        adUnitId = Interstitial_Level_IOS;
 #endif
-        }
 
         if (isTest)
         {
@@ -81,35 +103,50 @@ public class AdMobManager : MonoBehaviour
 #endif
         }
 
-        Debug.Log("[admobmanager][showad] " + adUnitId);
+        interstitialAd = new InterstitialAd(adUnitId);
+        Debug.Log("[interstitialAd][adUnitId] " + adUnitId);
 
-
-        if (adUnitId == "")
-        {
-            Debug.LogError("[admobmanager][showad] adUnitId does not exist");
-            return;
-        }
-
-        InterstitialAd ad = new InterstitialAd(adUnitId);
-        ad.OnAdLoaded += (object sender, System.EventArgs args) => {
-            ad.Show();
+        interstitialAd.OnAdLoaded += (object sender, System.EventArgs args) => {
+            GameAnalytics.NewAdEvent(GAAdAction.Loaded, GAAdType.Interstitial, "admob", "OnAdLoaded");
         };
+
+        interstitialAd.OnAdOpening += (object sender, System.EventArgs args) => {
+            GameAnalytics.NewAdEvent(GAAdAction.Show, GAAdType.Interstitial, "admob", "OnAdOpening");
+        };
+
+        interstitialAd.OnAdClosed += (object sender, System.EventArgs args) => {
+            LoadInterstitialAd();
+        };
+
+        interstitialAd.OnAdDidRecordImpression += (object sender, System.EventArgs args) => {
+            GameAnalytics.NewAdEvent(GAAdAction.Show, GAAdType.Interstitial, "admob", "OnAdDidRecordImpression");
+        };
+
+        interstitialAd.OnAdFailedToShow += (object sender, AdErrorEventArgs adErrorEventArgs) => {
+            GameAnalytics.NewAdEvent(GAAdAction.FailedShow, GAAdType.Interstitial, "admob", adErrorEventArgs.ToString());
+            LoadInterstitialAd();
+        };
+
+        interstitialAd.OnAdFailedToLoad += (object sender, AdFailedToLoadEventArgs adFailedToLoadEventArgs) => {
+            GameAnalytics.NewAdEvent(GAAdAction.FailedShow, GAAdType.Interstitial, "admob", adFailedToLoadEventArgs.ToString());
+            ClearInterstitialAd();
+        };
+
         AdRequest request = new AdRequest.Builder().Build();
-        ad.LoadAd(request);
+        interstitialAd.LoadAd(request);
+        GameAnalytics.NewAdEvent(GAAdAction.Request, GAAdType.Interstitial, "admob", "OnRequested");
     }
 
-    public void ShowRewardedAd(RewardedInterstitialAdType adType, UnityAction onUserEarnedReward)
-    {
+    private void LoadRewardedAd() {
+        ClearRewardedAd();
+
         string adUnitId = "";
 
-        if (adType == RewardedInterstitialAdType.Hint)
-        {
 #if UNITY_ANDROID
-            adUnitId = Rewarded_Hint_Android;
+        adUnitId = Rewarded_Hint_Android;
 #elif UNITY_IOS
-            adUnitId = Rewarded_Hint_IOS;
+        adUnitId = Rewarded_Hint_IOS;
 #endif
-        }
 
         if (isTest)
         {
@@ -120,24 +157,73 @@ public class AdMobManager : MonoBehaviour
 #endif
         }
 
-        Debug.Log("[admobmanager][showrewardedad] " + adUnitId);
+        rewardedAd = new RewardedAd(adUnitId);
+        Debug.Log("[rewardedAd][adUnitId] " + adUnitId);
 
-        if (adUnitId == "")
+        rewardedAd.OnAdLoaded += (object sender, System.EventArgs args) => {
+            GameAnalytics.NewAdEvent(GAAdAction.Loaded, GAAdType.RewardedVideo, "admob", "OnAdLoaded");
+        };
+
+        rewardedAd.OnAdOpening += (object sender, System.EventArgs args) => {
+            GameAnalytics.NewAdEvent(GAAdAction.Show, GAAdType.RewardedVideo, "admob", "OnAdOpening");
+        };
+
+        rewardedAd.OnAdDidRecordImpression += (object sender, System.EventArgs args) => {
+            GameAnalytics.NewAdEvent(GAAdAction.Show, GAAdType.RewardedVideo, "admob", "OnAdDidRecordImpression");
+        };
+
+        rewardedAd.OnAdClosed += (object sender, System.EventArgs args) => {
+            LoadRewardedAd();
+        };
+
+        rewardedAd.OnAdFailedToShow += (object sender, AdErrorEventArgs adErrorEventArgs) => {
+            GameAnalytics.NewAdEvent(GAAdAction.FailedShow, GAAdType.RewardedVideo, "admob", adErrorEventArgs.ToString());
+            LoadRewardedAd();
+        };
+
+        rewardedAd.OnAdFailedToLoad += (object sender, AdFailedToLoadEventArgs adFailedToLoadEventArgs) => {
+            GameAnalytics.NewAdEvent(GAAdAction.FailedShow, GAAdType.RewardedVideo, "admob", adFailedToLoadEventArgs.ToString());
+            ClearRewardedAd();
+        };
+
+        AdRequest request = new AdRequest.Builder().Build();
+        rewardedAd.LoadAd(request);
+        GameAnalytics.NewAdEvent(GAAdAction.Request, GAAdType.RewardedVideo, "admob", "OnRequested");
+    }
+
+    public void ShowAd(InterstitialAdType adType)
+    {
+        if (interstitialAd == null)
         {
-            Debug.LogError("[admobmanager][showrewardedad] adUnitId does not exist");
+            LoadInterstitialAd();
             return;
         }
 
+        if (!interstitialAd.IsLoaded()) return;
 
-        RewardedAd ad = new RewardedAd(adUnitId);
-        ad.OnUserEarnedReward += (object sender, Reward args) =>
+        interstitialAd.Show();
+        
+    }
+
+    public void ShowRewardedAd(RewardedInterstitialAdType adType, UnityAction onUserEarnedReward)
+    {
+        if (rewardedAd == null)
         {
             onUserEarnedReward();
+            LoadRewardedAd();
+            return;
+        }
+
+        if (!rewardedAd.IsLoaded()) {
+            onUserEarnedReward();
+            return;
+        }
+
+        rewardedAd.OnUserEarnedReward += (object sender, Reward reward) => {
+            onUserEarnedReward();
+            GameAnalytics.NewAdEvent(GAAdAction.RewardReceived, GAAdType.RewardedVideo, "admob", adType.ToString());
         };
-        ad.OnAdLoaded += (object sender, System.EventArgs args) => {
-            ad.Show();
-        };
-        AdRequest request = new AdRequest.Builder().Build();
-        ad.LoadAd(request);
+
+        rewardedAd.Show();
     }
 }
